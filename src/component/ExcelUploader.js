6488dca4c857be3dde81bdc7a117aac00f38ee35
kerbong <kerbong@gmail.com>
2022-12-15 21:05:32 +0900

    limit excel file size and duty expl

--- src/component/ExcelUploader.js
+++ src/component/ExcelUploader.js
@@ -17,49 +17,66 @@ const ExcelUploader = (props) => {
     let class_students = [];
     if (input.files[0] !== undefined) {
       let reader = new FileReader();
+      let isBig = false;
+      reader.onprogress = (e) => {
+        //엑셀파일 크기가 100KB이상이면(20반이상;)
+        if (e.total > 100000) {
+          isBig = true;
+        }
+      };
       reader.onload = function () {
-        try {
-          let data = reader.result;
-          let workBook = read(data, { type: "binary" });
+        //파일 크기가 적당하면
+        if (!isBig) {
+          try {
+            let data = reader.result;
+            let workBook = read(data, { type: "binary" });
 
-          //시트 각각을 작업하기
-          workBook.SheetNames.forEach((sheetName) => {
-            // 시트마다 줄을 객체로 저장하기
-            let rows = [...utils.sheet_to_json(workBook.Sheets[sheetName])];
-            let new_rows = [];
-            rows.forEach((row) => {
-              new_rows.push({
-                exClass: +row["반"],
-                birthday: +row["생년월일"],
-                num: +row["번호"],
-                gender: row["성별"],
-                name: row["이름"],
-                score: +row["총점"],
-                note: row["비고"] || "",
-                teamWork: row["협동"] || "",
+            //시트 각각을 작업하기
+            workBook.SheetNames.forEach((sheetName) => {
+              // 시트마다 줄을 객체로 저장하기
+              let rows = [...utils.sheet_to_json(workBook.Sheets[sheetName])];
+              let new_rows = [];
+              rows.forEach((row) => {
+                new_rows.push({
+                  exClass: +row["반"],
+                  birthday: +row["생년월일"],
+                  num: +row["번호"],
+                  gender: row["성별"],
+                  name: row["이름"],
+                  score: +row["총점"],
+                  note: row["비고"] || "",
+                  teamWork: row["협동"] || "",
+                });
               });
-            });
-            //스코어 높은순으로 정렬하기
-            new_rows.sort((a, b) => {
-              return b["score"] - a["score"];
+              //스코어 높은순으로 정렬하기
+              new_rows.sort((a, b) => {
+                return b["score"] - a["score"];
+              });
+
+              class_students.push([...new_rows]);
             });
 
-            class_students.push([...new_rows]);
-          });
+            setClassStudents([...class_students]);
+            //학생정보가 저장되면 로컬스토리지에 문자로 저장해두기
+            // localStorage.setItem("randomStudents", JSON.stringify(new_rows));
+          } catch (error) {
+            Swal.fire({
+              icon: "error",
+              title: "업로드불가",
+              text: "엑셀파일에 비어있는 행, 열이 있는지 확인해주세요! 문제가 지속될 경우 알려주세요!",
+            });
+          }
 
-          setClassStudents([...class_students]);
-          //학생정보가 저장되면 로컬스토리지에 문자로 저장해두기
-          // localStorage.setItem("randomStudents", JSON.stringify(new_rows));
-        } catch (error) {
+          //파일 크기가 너무 큰 경우
+        } else {
           Swal.fire({
             icon: "error",
             title: "업로드불가",
-            text: "엑셀파일에 비어있는 행, 열이 있는지 확인해주세요! 문제가 지속될 경우 알려주세요!",
+            text: "엑셀파일의 크기가 너무 크네요! 중복된 시트가 없는지 확인해주세요. 문제가 지속되시면 kerbong@gmail.com으로 알려주세요!",
           });
+          return false;
         }
       };
-      //   console.log(Array.isArray(class_students));
-      //   console.log(class_students);
 
       reader.readAsBinaryString(input.files[0]);
     } else {
